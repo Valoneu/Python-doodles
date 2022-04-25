2ea598412b0c5affd061c3bad9ec1133a107658f
Valoneu <filip@becicka.cz>
2022-04-25 21:47:12 +0200

    calculator 1.0

--- Calculator.py
+++ Calculator.py
@@ -121,56 +121,33 @@ if prognum == 1:
         
     elif prog1mod == 3:
         print()
-        print("selected y = |ax| + b","select action","1 -> calculate a,b from 2 points","2 -> calculates y from x","3 -> calculates x from y",sep = "\n")
+        print("selected y = |ax| + b","select action","1 -> calculates y from x","2 -> calculates x from y",sep = "\n")
         
         actnum = int(input("action: "))
         
         if actnum == 1:
-            print()
-            
-            print("y = |a * x| + b")
-            x1 = int(input("input x1: "))
-            y1 = int(input("input y1: "))
-            x2 = int(input("input x2: "))
-            y2 = int(input("input y2: "))
-            
-            print()
-            print ("point one:[",x1,y1,"],point2 :[",x2,y2,"]")
-            print()
-            
-            a = (y1-y2) / (x1-x2)
-            print ("a is:", a)
-            print()
-            
-            b = y1 - x1 * a
-            print ("b is:", b)
-            print()
-            
-            print ("full is y = |",a,"* x | +",b)
-            
-        elif actnum == 2:
             print()    
             
-            print("y = a * x + b")
+            print("y = |a * x| + b")
             a = int(input("input a: "))
             x = int(input("input x: "))
             b = int(input("input b: "))
             
-            y = a * x + b
+            y = abs(a * x) + b
     
             print ("value y is",y,"at x",x)
             
-        elif actnum == 3:
+        elif actnum == 2:
             print()    
             
             print("x = (y - b) / a")
             a = int(input("input a: "))
             y = int(input("input y: "))
             b = int(input("input b: "))
-            
+
             x = (y - b) / a
     
-            print ("value x is",x,"at y",y)
+            print ("value x is",-x,",",+x,"at y",y)
          
         else:
             print()
@@ -178,8 +155,37 @@ if prognum == 1:
         
     elif prog1mod == 4:
         print()
-        print("selected y = |ax + b|")
-        print("select action")   
+        print("selected y = |ax + b|","select action","1 -> calculates y from x","2 -> calculates x from y",sep = "\n")
+        
+        actnum = int(input("action: "))
+        
+        if actnum == 1:
+            print()    
+            
+            print("y = |a * x + b|")
+            a = int(input("input a: "))
+            x = int(input("input x: "))
+            b = int(input("input b: "))
+            
+            y = abs(a * x + b)
+    
+            print ("value y is",y,"at x",x)
+            
+        elif actnum == 2:
+            print()    
+            
+            print("x = (y - b) / a")
+            a = int(input("input a: "))
+            y = int(input("input y: "))
+            b = int(input("input b: "))
+
+            x = (y - b) / a
+    
+            print ("value x is",-x,",",+x,"at y",y)
+         
+        else:
+            print()
+            print("action num",actnum,"doesent exist") 
         
     else:
         print()
