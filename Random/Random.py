0e8398a0f81f57a2cf79de108152f16088fa3310
Valoneu <filip@becicka.cz>
2022-09-18 01:24:33 +0200

    Update Random.py

--- Random/Random.py
+++ Random/Random.py
@@ -1,5 +1,40 @@
-import math
+#stake dice sim
 
+from audioop import bias
+import random
 
-for x in range (1,41):
-    print(x ** x)
+Broke = False
+Balance = 10000
+BetSize = 1
+StartingBetSize = 1
+Over = True
+Bias = 550
+RollNumber = 0
+
+while Broke == False :
+    if Over == True :
+        RandomNumber = random.randrange(0, 1000)
+        if RandomNumber >= Bias :
+            Balance = Balance + (BetSize * 2.2)
+            BetSize = StartingBetSize
+            RollNumber = RollNumber + 1
+            print (RollNumber,"",BetSize,"",Balance,"",RandomNumber)
+        else :
+            Balance = Balance - BetSize
+            BetSize = BetSize * 1.65
+            RollNumber = RollNumber + 1
+            print (RollNumber,"",BetSize,"",Balance,"",RandomNumber)
+    if Over == False :
+        RandomNumber = random.randrange(0, 1000)
+        if RandomNumber <= Bias :
+            Balance = Balance + (BetSize * 2.2)
+            BetSize = 1
+            RollNumber = RollNumber + 1
+            print (RollNumber,"",BetSize,"",Balance,"",RandomNumber)
+        else :
+            Balance = Balance - BetSize
+            BetSize = BetSize * 1.65
+            RollNumber = RollNumber + 1
+            print (RollNumber,"",BetSize,"",Balance,"",RandomNumber)
+    if RollNumber == 100000 or Balance <= 0:
+        Broke = True
